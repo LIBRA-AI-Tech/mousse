9433dd37998053bdd30240af4efd05a0da8a9043
Pantelis Mitropoulos <pantelis.mitropoulos@libramli.ai>
2024-12-12 16:36:09 +0200

    Roboto fonts

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,10 +7,10 @@ import App from './app';
 import store from './app/store';
 import './index.css';
 
-// import '@fontsource/roboto/300.css';
-// import '@fontsource/roboto/400.css';
-// import '@fontsource/roboto/500.css';
-// import '@fontsource/roboto/700.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
