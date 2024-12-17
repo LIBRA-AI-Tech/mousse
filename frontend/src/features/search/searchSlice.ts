77c74fc715cfa66475cf3b38495cddeec6e2e741
Pantelis Mitropoulos <pantelis.mitropoulos@libramli.ai>
2024-12-17 11:53:33 +0200

    Use cache for previous pages

--- frontend/src/features/search/searchSlice.ts
+++ frontend/src/features/search/searchSlice.ts
@@ -1,7 +1,10 @@
 import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { FeatureCollection, Feature } from 'geojson';
-import { RecordSearchRequest, recordSearch } from '../../services/recordsApi';
+import { RecordSearchRequest, recordSearch, recordSearchResponse } from '../../services/recordsApi';
 import { FilterValuesType } from '../../types';
+import { RootState } from '../../app/store';
+
+const CACHE_SIZE = 30;
 
 interface Records {
   data: FeatureCollection | null;
@@ -18,6 +21,7 @@ interface SearchState {
   features: Feature[]
   status: 'idle' | 'loading' | 'succeeded' | 'failed';
   error: string | null;
+  cache: Record<number, Records>;
 }
 
 const initialRecords: Records = {
@@ -42,13 +46,38 @@ const initialState: SearchState = {
   features: [],
   status: 'idle',
   error: null,
+  cache: {},
 }
 
 // Thunks
-export const fetchRecords = createAsyncThunk('records/search', async (body: RecordSearchRequest) => {
-  const response = await recordSearch(body);
-  return response.data;
-});
+export const fetchRecords = createAsyncThunk('records/search', 
+  async (body: RecordSearchRequest, { getState, rejectWithValue }) => {
+    const state = getState() as RootState;
+
+    const { page } = body;
+    if (page && state.search.cache[page]) {
+      return rejectWithValue({ usedCache: true, data: state.search.cache[page] });
+    }
+    const response = await recordSearch(body);
+    return response.data;
+  }
+);
+
+const initiateNewSearch = (state: SearchState) => {
+  state.currentPage = 1;
+  state.pageCount = 1;
+  state.cache = {};
+}
+
+const addToCache = (state: SearchState, payload: recordSearchResponse) => {
+  const current_cache: Record<number, Records> = Object.entries(state.cache)
+    .slice(Math.max(0, Object.keys(state.cache).length - CACHE_SIZE))
+    .reduce((obj, [key, value]) => ({
+      ...obj,
+      [key]: value,
+    }), {});
+  state.cache = {...current_cache, [payload.page]: payload};
+}
 
 const searchSlice = createSlice({
   name: 'search',
@@ -61,24 +90,20 @@ const searchSlice = createSlice({
     submitSearch: (state, action: PayloadAction<{query: string, filterValues: FilterValuesType}>) => {
       state.query = action.payload.query;
       state.filterValues = action.payload.filterValues;
-      state.currentPage = 1;
-      state.pageCount = 1;
+      initiateNewSearch(state);
     },
     addLayer: (state, action: PayloadAction<Feature>) => {
       state.features = [...state.features, action.payload];
-      state.currentPage = 1;
-      state.pageCount = 1;
+      initiateNewSearch(state);
     },
     editLayer: (state, action: PayloadAction<Feature>) => {
       state.features = state.features
         .map(feature => feature.id === action.payload.id ? action.payload : feature);
-      state.currentPage = 1;
-      state.pageCount = 1;
+        initiateNewSearch(state);
     },
     deleteLayer: (state, action: PayloadAction<number>) => {
       state.features = state.features.filter(feature => feature.id !== action.payload);
-      state.currentPage = 1;
-      state.pageCount = 1;
+      initiateNewSearch(state);
     },
     resetLayer: (state) => {
       state.features = [];
@@ -92,13 +117,20 @@ const searchSlice = createSlice({
       .addCase(fetchRecords.fulfilled, (state, action) => {
         state.status = 'succeeded';
         state.error = null;
+        addToCache(state, action.payload);
         state.records = action.payload;
         state.pageCount = Math.max(state.pageCount, action.payload.page + Number(action.payload.hasMore))
       })
       .addCase(fetchRecords.rejected, (state, action) => {
-        Object.assign(state, initialState);
-        state.status = 'failed';
-        state.error = action.error.message || null;
+        if (action.payload?.usedCache) {
+          state.status = 'succeeded';
+          state.error = null;
+          state.records = action.payload.data;
+        } else {
+          Object.assign(state, initialState);
+          state.status = 'failed';
+          state.error = action.error.message || null;
+        }
       });
   }
 });
