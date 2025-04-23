960a00d9b37ad4b1c9f3a3a891b25a44f14a3480
Kiriakos Sidiropoulos <kiriakos.sidiropoulos@libramli.ai>
2025-04-23 15:52:39 +0300

    add tooltip in clusterGrid elementCount

--- frontend/src/app/components/clusteredGrid.tsx
+++ frontend/src/app/components/clusteredGrid.tsx
@@ -1,4 +1,4 @@
-import { Paper, Typography } from "@mui/material"
+import { Paper, Tooltip, Typography } from "@mui/material"
 import { Grid } from "@mui/system"
 import { RootState, useAppDispatch } from "../store"
 import { useSelector } from "react-redux"
@@ -13,12 +13,14 @@ const ClusteredGrid = () => {
   }
 
   return (
-    <Grid sx={{p: '1rem'}} spacing={2}>
+    <Grid sx={{p: '1rem', display: 'grid', gridTemplateColumns: {sm: '1fr', lg: '1fr 1fr'}, gap: '1rem'}} spacing={2}>
       {clusters && clusters.map(r => (
         <Paper variant="outlined" key={r.id} sx={{padding: '.8rem .7rem', boxShadow: '0px 0px 10px rgba(0,0,0,0.1)'}}>
-          <Grid>
+          <Grid container alignItems='center' justifyContent='space-between'>
             <Typography component="h6" variant="h6" onClick={() => handleClusterHover(r.id)} sx={{"&:hover": {textDecoration: 'underline', textDecorationStyle: 'wavy', textDecorationColor: theme => theme.palette.grey[600], cursor: 'pointer'}}}>{r.summary}</Typography>
-            <Typography component="h5" variant="h6" sx={{background: theme => theme.palette.grey[600], color: 'white', borderRadius: '.5rem', boxShadow: '2px 2px 10px rgba(0, 0, 0, 0.2)', width: 'max-content', padding: '.1rem .5rem', marginBlockEnd: '1rem'}}>{r.elementCount}</Typography>
+            <Tooltip title={`There are ${r.elementCount} elements in this cluster`}>
+              <Typography component="h5" variant="h6" sx={{background: theme => theme.palette.grey[600], color: 'white', borderRadius: '.5rem', boxShadow: '2px 2px 10px rgba(0, 0, 0, 0.2)', width: 'max-content', padding: '.1rem .5rem', marginBlockEnd: '1rem'}}>{r.elementCount}</Typography>
+            </Tooltip>
           </Grid>
           <Typography>{r.representativeTitle}</Typography>
           <Typography sx={{width: '35ch'}}></Typography>
